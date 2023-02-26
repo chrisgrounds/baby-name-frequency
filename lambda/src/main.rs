ef4fbd988671bab5c6fccd57256403d6d5c9d2b5
Chris Bacon <chrisbacon2009@hotmail.com>
2023-02-26 21:37:20 +0000

    Change path param to query string and add gender

--- lambda/src/main.rs
+++ lambda/src/main.rs
@@ -7,55 +7,60 @@ mod consolidated_record;
 mod csv_data;
 
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    let path_params = event.path_parameters();
+    let path_params = event.query_string_parameters();
+    let name_param: Option<&str> = path_params.first("name");
+    let gender_param: Option<&str> = path_params.first("gender");
 
-    match path_params.first("name") {
-        None => Err("Missing query param 'name'".into()),
-        Some(name) => {
-            println!("Name is {}", name);
+    match name_param {
+        None => Err("Missing query param 'name' or 'gender'".into()),
+        Some(name) => match gender_param {
+            None => Err("Missing query param 'name' or 'gender'".into()),
+            Some(gender) => {
+                println!("Gender is {}", gender);
 
-            let config = aws_config::from_env().region("eu-west-1").load().await;
-            let filename = "girls_baby_names_1996_2021.csv";
+                let config = aws_config::from_env().region("eu-west-1").load().await;
+                let filename = &format!("{}_baby_names_1996_2021.csv", gender.to_string());
 
-            let s3_client = aws_sdk_s3::Client::new(&config);
+                let s3_client = aws_sdk_s3::Client::new(&config);
 
-            let data = s3_client
-                .get_object()
-                .bucket("baby-names-1996-2021")
-                .key(filename)
-                .send()
-                .await
-                .expect(&format!("Failed to get file {}", filename));
+                let data = s3_client
+                    .get_object()
+                    .bucket("baby-names-1996-2021")
+                    .key(filename)
+                    .send()
+                    .await
+                    .expect(&format!("Failed to get file {}", filename));
 
-            let s3_result = data.body.collect().await.unwrap().into_bytes();
-            let response = std::str::from_utf8(&s3_result).unwrap();
-            let mut rdr = csv::ReaderBuilder::new().from_reader(response.as_bytes());
+                let s3_result = data.body.collect().await.unwrap().into_bytes();
+                let response = std::str::from_utf8(&s3_result).unwrap();
+                let mut rdr = csv::ReaderBuilder::new().from_reader(response.as_bytes());
 
-            for record in rdr.records() {
-                let row = record.unwrap();
+                for record in rdr.records() {
+                    let row = record.unwrap();
 
-                if row.iter().any(|field| field == name) {
-                    let baby_name_record: Result<csv_data::CsvData, csv::Error> =
-                        row.deserialize(None);
+                    if row.iter().any(|field| field == name) {
+                        let baby_name_record: Result<csv_data::CsvData, csv::Error> =
+                            row.deserialize(None);
 
-                    if let Ok(csv_data) = baby_name_record {
-                        let consolidated_record: ConsolidatedRecord =
-                            consolidated_record::calculate(&csv_data);
-                        println!("{:?}", consolidated_record);
+                        if let Ok(csv_data) = baby_name_record {
+                            let consolidated_record: ConsolidatedRecord =
+                                consolidated_record::calculate(&csv_data);
+                            println!("{:?}", consolidated_record);
 
-                        let resp: Response<Body> = Response::builder()
-                            .status(200)
-                            .header("content-type", "text/html")
-                            .body(format!("{}", consolidated_record.total_count).into())
-                            .map_err(Box::new)?;
+                            let resp: Response<Body> = Response::builder()
+                                .status(200)
+                                .header("content-type", "text/html")
+                                .body(format!("{}", consolidated_record.total_count).into())
+                                .map_err(Box::new)?;
 
-                        return Ok::<Response<Body>, Error>(resp);
+                            return Ok::<Response<Body>, Error>(resp);
+                        };
                     };
-                };
-            }
+                }
 
-            return Err("Uh oh".into());
-        }
+                return Err("Uh oh".into());
+            }
+        },
     }
 }
 
